e23661b9d0120da346944506f8bf5802fc72412c
xstater <a166224@163.com>
2019-10-08 03:33:49 +0800

    回退至旧版本

--- XSignal.h
+++ XSignal.h
@@ -1,57 +1,176 @@
 #ifndef _XSIGNAL_H_
 #define _XSIGNAL_H_
 
-#include <vector>
-
-#define XSignalGroup(name) \
-struct name
-#define __signal__ virtual
-#define XSignal(name,signal_group) \
-    xsignal::Signal<signal_group> name
-
-#define XSlotGroup(name,signal_group) \
-struct name : public signal_group
-#define __slot__ virtual
-#define XSlot(name,slot_group) slot_group name
-
-#define XConnect(slot,signal) signal += &slot
-#define XDisconnect(slot,signal) signal -= &slot
-#define XEmit(signal,func) \
-    for(auto ptr:signal)ptr->func
+#include <functional>
+#include <type_traits>
+#include <memory>
+#include <list>
 
 namespace xsignal{
     template<class Type>
-    class Signal{
+    class Signal{};
+
+    template <class Type>
+    class Connection{};
+
+    template<class ReturnType,class...ArgsType>
+    class Connection<ReturnType(ArgsType...)>{
     public:
-        using iterator = typename std::vector<Type*>::iterator;
-        //using const_iterator = std::list<Type*>::const_iterator;
-    
-        Signal() = default;
-        ~Signal() = default;
-        
-        Signal &operator+=(Type *ptr){
-            m_slots.push_back(ptr);
-            return *this;
+
+        using SignalType = Signal<ReturnType(ArgsType...)>;
+        using iter_type = typename SignalType::iter_type;
+
+        explicit Connection(SignalType &signal,iter_type itr)
+                :m_signal(signal),m_itr(itr){}
+        Connection(const Connection &) = delete;
+        Connection(Connection &&) = default;
+        ~Connection() = default;
+
+        Connection &operator=(const Connection &) = delete;
+        Connection &operator=(Connection &&) = default;
+
+        void disconnect()noexcept{
+            m_signal.__remove(m_itr);
         }
-        Signal &operator-=(Type *ptr){
-            for(auto itr = m_slots.begin();itr != m_slots.end();++itr){
-                if(*itr == ptr){
-                    m_slots.erase(itr);
-                    break;
-                }
-            }
-            return *this;
-        }
-        
-        iterator begin(){
-            return m_slots.begin();
-        }
-        iterator end(){
-            return m_slots.end();
+
+        auto value()
+        -> typename std::enable_if<!std::is_void<ReturnType>::value,ReturnType>::type{
+            return m_itr->ret_value;
         }
     protected:
     private:
-        std::vector<Type*> m_slots;
+        SignalType &m_signal;
+        iter_type m_itr;
+    };
+
+    template<class...ArgsType>
+    class Connection<void(ArgsType...)>{
+    public:
+
+        using SignalType = Signal<void(ArgsType...)>;
+        using iter_type = typename SignalType::iter_type;
+
+        explicit Connection(SignalType &signal,iter_type itr)
+                :m_signal(signal),m_itr(itr){}
+        Connection(const Connection &) = delete;
+        Connection(Connection &&) = default;
+        ~Connection() = default;
+
+        Connection &operator=(const Connection &) = delete;
+        Connection &operator=(Connection &&) = default;
+
+        void disconnect()noexcept{
+            m_signal.__remove(m_itr);
+        }
+    protected:
+    private:
+        SignalType &m_signal;
+        iter_type m_itr;
+    };
+
+///ReturnType must have a default constructor!!!
+
+    template<class ReturnType,class...ArgsType>
+    class Signal<ReturnType(ArgsType...)>{
+    public:
+        struct Slot{
+            template <class Function>
+            explicit Slot(Function &&f)
+                    :func(std::forward<Function>(f)){}
+            std::function<ReturnType(ArgsType...)> func;
+            ReturnType ret_value;
+        };
+
+        using iter_type = typename std::list<Slot>::iterator;
+        using return_type = ReturnType;
+
+        Signal() = default;
+        ~Signal() = default;
+
+        void __remove(iter_type itr)noexcept{
+            m_slots.erase(itr);
+        }
+
+        template<class Function>
+        auto connect(Function &&func)
+        -> Connection<ReturnType(ArgsType...)>{
+            m_slots.emplace_back(std::forward<Function>(func));
+            return Connection<ReturnType(ArgsType...)>(*this,std::prev(m_slots.end()));
+        }
+        template<class Function>
+        auto operator<<(Function &&func)
+        -> Signal &{
+            m_slots.emplace_back(std::forward<Function>(func));
+            return *this;
+        }
+
+        template <class...ArgsType2>
+        auto emit(ArgsType2&&...args)
+        -> typename std::enable_if<!std::is_void<ReturnType>::value,ReturnType>::type{
+            for(auto &slot:m_slots){
+                slot.ret_value = slot.func(std::forward<ArgsType2>(args)...);
+            }
+            return m_slots.begin()->ret_value;
+        }
+
+        template <class...ArgsType2>
+        auto operator()(ArgsType2&&...args)
+        -> typename std::enable_if<!std::is_void<ReturnType>::value,ReturnType>::type{
+            return emit(std::forward<ArgsType2>(args)...);
+        }
+    protected:
+    private:
+        std::list<Slot> m_slots;
+    };
+
+    template<class...ArgsType>
+    class Signal<void(ArgsType...)>{
+    public:
+        struct Slot{
+            template <class Function>
+            explicit Slot(Function &&f)
+                    :func(std::forward<Function>(f)){}
+            std::function<void(ArgsType...)> func;
+        };
+
+        using iter_type = typename std::list<Slot>::iterator;
+        using return_type = void;
+
+        Signal() = default;
+        ~Signal(){
+            m_slots.clear();
+        }
+
+        void __remove(iter_type itr){
+            m_slots.erase(itr);
+        }
+
+        template<class Function>
+        auto connect(Function &&func)
+        -> Connection<void(ArgsType...)>{
+            m_slots.emplace_back(std::forward<Function>(func));
+            return Connection<void(ArgsType...)>(*this,std::prev(m_slots.end()));
+        }
+        template<class Function>
+        auto operator<<(Function &&func)
+        -> Signal &{
+            m_slots.emplace_back(std::forward<Function>(func));
+            return *this;
+        }
+
+        template <class...ArgsType2>
+        void emit(ArgsType2&&...args) {
+            for(auto &itr:m_slots){
+                itr.func(std::forward<ArgsType2>(args)...);
+            }
+        }
+        template <class...ArgsType2>
+        void operator()(ArgsType2&&...args) {
+            emit(std::forward<ArgsType2>(args)...);
+        }
+    protected:
+    private:
+        std::list<Slot> m_slots;
     };
 }
 
